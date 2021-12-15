c62909906493feef2c607b3365611e7d22725ae4
tom <tom.g.woodward@gmail.com>
2021-12-15 10:06:29 -0500

    add support for archive override in release

--- spec/lib/rex/release_spec.rb
+++ spec/lib/rex/release_spec.rb
@@ -5,22 +5,39 @@ RSpec.describe Rex::Release do
   describe '#books' do
     let(:book1) { { "uid1" => { "defaultVersion" => "1.2"} } }
     let(:book2) { { "uid2" => { "defaultVersion" => "1.1"} } }
-    let(:data) { { books: book1.merge(book2) } }
+    let(:book3) { { "uid3" => { "defaultVersion" => "1.3", "archiveOverride" => "/apps/archive/20210713.205645"} } }
+    let(:data) { { books: book1.merge(book2).merge(book3) } }
     let(:instance) { described_class.new(id: 'id', data: data, config: config) }
 
     context 'when there is no config data' do
       let(:config) { OpenStruct.new(pipeline: nil) }
 
-      it 'does not show a pipeline prefix on IDs' do
-        expect(instance.books.map(&:id)).to contain_exactly("uid1@1.2", "uid2@1.1")
+      it 'has the right number of books' do
+        expect(instance.books.length).to equal(3)
+      end
+
+      it 'does not show config pipeline prefix on IDs' do
+        expect(instance.books.map(&:id)).to include("uid1@1.2", "uid2@1.1")
+      end
+
+      it 'shows overrid pipeline prefix on IDs' do
+        expect(instance.books.map(&:id)).to include("20210713.205645/uid3@1.3")
       end
     end
 
     context 'when there is config data do' do
       let(:config) { OpenStruct.new(pipeline: 'foo')}
 
-      it 'uses the pipeline as a prefix' do
-        expect(instance.books.map(&:id)).to contain_exactly("foo/uid1@1.2", "foo/uid2@1.1")
+      it 'has the right number of books' do
+        expect(instance.books.length).to equal(3)
+      end
+
+      it 'uses the config pipeline as a prefix' do
+        expect(instance.books.map(&:id)).to include("foo/uid1@1.2", "foo/uid2@1.1")
+      end
+
+      it 'uses the archive override as pipeline in the prefix' do
+        expect(instance.books.map(&:id)).to include("20210713.205645/uid3@1.3")
       end
     end
   end
